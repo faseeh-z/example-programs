d67ce63c2fb33393e881e3f144c48316088f2a34
Faseeh Zaman <faseeh1080@outlook.com>
2024-12-29 11:22:28 +0530

    Update JavaScript example.

--- languages/javascript/01-basics/script.js
+++ languages/javascript/01-basics/script.js
@@ -1,15 +1,3 @@
-// Variables
-let userName = "Faseeh"; // In JavaScript, name is a special property of many built-in objects. So we can't use it as a variable name.
-const G = 6.67 * (10 ** -11);
-
-// Functions
-function greet(theName) {
-    console.log("Hello " + theName);
-    // Return statement is not mandatory.
-}
-
-greet(userName);
-
 document.getElementById("donate-btn").addEventListener("click", function() {
 
      // Remove the button on click.
